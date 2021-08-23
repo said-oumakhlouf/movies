fd3f35224f265934b96c53e1d978559dfc391d4e
said-oumakhlouf <oumakhlouf.said@gmail.com>
2021-08-23 17:40:01 +0200

    Movie Application

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect }from 'react';
+
+// Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';  
+// Components
+
+// Hook 
+import { useHomeFetch } from '../hooks/useHomeFetch';
+// Image 
+import NoImage from '../images/no_image.jpg';
+
+
+const Home = () => {
+    const { state, loading, error} = useHomeFetch();
+    
+    console.log(state);
+
+    return (
+        <div>
+            Accueil
+        </div>
+    );
+};
+
+export default Home;
