e0372091563c89440e4037f85b0eadbb6af2a60d
said-oumakhlouf <oumakhlouf.said@gmail.com>
2021-09-08 19:28:09 +0200

    Composant MovieInfo

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,6 +5,8 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // Components 
 import Grid from './Grid';
 import Spinner from './Spinner';
+import BreadCrumb from './BreadCrumb';
+import MovieInfo from './MovieInfo';
 // Hook 
 import { useMovieFetch } from '../hooks/useMovieFetch';
 // Image 
@@ -13,10 +15,13 @@ const Movie = () => {
     const { movieId } = useParams();
     const { state: movie, loading, error } = useMovieFetch(movieId);
 
-    console.log(movie);
+    if(loading) return <Spinner />;
+    if(error) return <div>Quelque chose ne vas pas ...</div>
+
     return (
         <>
-            <div>Movie</div>
+            <BreadCrumb movieTitle={movie.original_title} />
+            <MovieInfo movie={movie} />
         </>
     )
 };
