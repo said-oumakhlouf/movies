7cb79d35ca0df3fffa6a0bacdef33dc342632a0a
said-oumakhlouf <oumakhlouf.said@gmail.com>
2021-09-09 10:48:33 +0200

    Actor components effectué

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,11 +8,13 @@ import Spinner from './Spinner';
 import BreadCrumb from './BreadCrumb';
 import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
 
 // Hook 
 import { useMovieFetch } from '../hooks/useMovieFetch';
 // Image 
 import NoImage from '../images/no_image.jpg';
+
 const Movie = () => {
     const { movieId } = useParams();
     const { state: movie, loading, error } = useMovieFetch(movieId);
@@ -25,6 +27,20 @@ const Movie = () => {
             <BreadCrumb movieTitle={movie.original_title} />
             <MovieInfo movie={movie} />
             <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+            <Grid header='Actors'>
+                {movie.actors.map(actor => (
+                    <Actor 
+                        key={actor.credit_id} 
+                        name={actor.name} 
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path ?
+                            `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                            : NoImage
+                        } 
+                    />
+                ))} 
+            </Grid>
         </>
     )
 };
