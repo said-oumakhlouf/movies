8717a300ee0f1bd52d8412af131e632b065773d0
said-oumakhlouf <oumakhlouf.said@gmail.com>
2021-09-10 19:55:20 +0200

    sessionStorage pour les films

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import API from "../API";
+import { isPersistedState } from '../helpers';
+
 
 export const useMovieFetch = movieId => {
     const [state, setState] = useState({});
@@ -29,11 +31,23 @@ export const useMovieFetch = movieId => {
             } catch (error) {
                 setError(true);
             }
+        };
+
+        const sessionState = isPersistedState(movieId);
+
+        if(sessionState) {
+            setState(sessionState);
+            setLoading(false);
+            return;
         }
 
         fetchMovie();
     }, [movieId]);
 
+    // Write to sessionStorage
+    useEffect(() => {
+        sessionStorage.setItem(movieId, JSON.stringify(state));
+    }, [movieId, state]);
     return { state, loading, error };
 
 }
