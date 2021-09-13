0bde2d0d466392243adf8bc368128b80b72f104a
said-oumakhlouf <oumakhlouf.said@gmail.com>
2021-09-13 22:27:16 +0200

    Refactoriser la page Home en class component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+// API
+import API from '../API';
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';  
 // Components
@@ -10,49 +11,98 @@ import Spinner from './Spinner';
 import SearchBar from './SearchBar';
 import Button from './Button';
 
-// Hook 
-import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image 
 import NoImage from '../images/no_image.jpg';
 
+const initialState = {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+};
 
-const Home = () => {
-    const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } = useHomeFetch();
+class Home extends Component {
+    state = {
+        movies: initialState,
+        searchTerm: '',
+        isLoadingMore: false,
+        loading: false,
+        error: false,
+    }
+
+    fetchMovies = async(page, searchTerm = '') => {
+        try {
+            this.setState({ error: false, loading: true });
+
+            const movies = await API.fetchMovies(searchTerm, page);
+
+            this.setState(prev => ({
+                ...prev,
+                movies: {
+                    ...movies,
+                    results:
+                        page > 1 ? [...prev.movies.results, ...movies.results] : [...movies.results]
+                },
+                loading: false,
+            }));
+
+        } catch (error) {
+            this.setState({ error: true, loading: false })
+        }
+    };
+
+
+    handleSearch = searchTerm => 
+        this.setState({ movies: initialState, searchTerm }, () => 
+            this.fetchMovies(1, this.state.searchTerm)
+        )
+
+    handleLoadMore = () =>
+        this.fetchMovies(this.state.movies.page + 1, this.state.searchTerm);
     
-    console.log(state);
+    
+    componentDidMount() {
+        this.fetchMovies(1);
+    }
 
-    if(error) return <div>Quelque chose ne va pas ...</div>
+    render() {
 
-    return (
-        <>
-            {!searchTerm && state.results[0] ? (
-                <HeroImage
-                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-                    title={state.results[0].original_title}
-                    text={state.results[0].overview}
-                /> 
-            ) : null}
-            <SearchBar setSearchTerm={setSearchTerm} />
-            <Grid header={searchTerm ? 'Résultat de la recherche' : 'Films Populaires'}>
-                {state.results.map(movie => (
-                    <Thumb 
-                        key={movie.id}
-                        clickable
-                        image= {
-                            movie.poster_path 
-                                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
-                                : NoImage
-                        }
-                        movieId={movie.id}
-                    />
-                ))}
-            </Grid>
-            {loading && <Spinner />}
-            {state.page < state.total_pages && !loading && (
-                <Button text='Charge en plus' callback={() => setIsLoadingMore(true)} />
-            )}
-        </>
-    );
+        const { searchTerm, movies, loading, error} = this.state;
+
+
+        if(error) return <div>Quelque chose ne va pas ...</div>
+
+        return (
+            <>
+                {!searchTerm && movies.results[0] ? (
+                    <HeroImage
+                        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movies.results[0].backdrop_path}`}
+                        title={movies.results[0].original_title}
+                        text={movies.results[0].overview}
+                    /> 
+                ) : null}
+                <SearchBar setSearchTerm={this.handleSearch} />
+                <Grid header={searchTerm ? 'Résultat de la recherche' : 'Films Populaires'}>
+                    {movies.results.map(movie => (
+                        <Thumb 
+                            key={movie.id}
+                            clickable
+                            image= {
+                                movie.poster_path 
+                                    ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                                    : NoImage
+                            }
+                            movieId={movie.id}
+                        />
+                    ))}
+                </Grid>
+                {loading && <Spinner />}
+                {movies.page < movies.total_pages && !loading && (
+                    <Button text='Charge en plus' callback={this.handleLoadMore} />
+                )}
+            </>
+        );
+    } 
 };
 
 export default Home;
